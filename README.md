e6e19331463795b21e6d9f9f5987bbb0a9053850
Griff-Master <griffinskyei6@gmail.com>
2026-03-06 12:34:03 +0300

    Initial commit - Number Guessing Game

---
+++ README.md
@@ -0,0 +1,46 @@
+:rocket: Number Guessing Game
+:video_game: How the game works 
+1. The player chooses difficulty
+2. The system generates secrete number
+3. The player enters his/her guesses
+4. Allows one to enter guesses until  he/she makes correct guess or runs out of attempts
+5. Keeps track of valid attempts
+6. The game provides feedback
+    - Too high!
+    - Too low!
+    - Warm/cold hints
+7. After winning the player can choose to play again
+
+:rocket: Optional features added
+1. :level_slider: Difficulty levels
+     - Easy (1➡️50)
+     - Normal (1➡️100)
+     - Hard (1➡️500)
+2. :timer_clock: Limited attempts 
+     - Easy ➡️ 7 attempts
+     - Normal ➡️ 10 attempts
+     - Hard ➡️ 15 attempts
+3. :fire: Warm/cold hints
+     - very close ➡️ within 5 numbers
+     - close ➡️ within 10 numbers
+     - far ➡️ otherwise
+4. :arrows_counterclockwise: Replay Option
+     - After winning the player can choose to play again
+
+:forward_arrow: How to Run
+1. Clone the repository
+    - Copy the Github repository URL
+    - Open IntelliJ
+    - Go to Files - New - project - project from version control
+    - Paste the URL
+    - clone
+2. Run NumberGuessgame
+
+:brain: Thinking Task
+1. When to use nested conditions
+    - Are used when second conditions should be checked when the first one is true
+    - Example ➡️ in login system check first if user exists then if password is correct
+2. When nested conditions makes code worse
+    - When there are many if statements inside each other for input validation
+3. :hammer_and_wrench: Technique to reduce nesting
+     - Use continue/return to exit early when conditions are invalid
---
+++ src/NumberGuessgame.java
@@ -0,0 +1,115 @@
+import java.util.Scanner;
+import java.util.Random;
+public class NumberGuessgame {
+    static Scanner scanner = new Scanner(System.in);
+    static Random random = new Random();
+
+    public static void main(String[] args) {
+        printWelcome();
+        while (playGame()) {
+
+        }
+        System.out.println("Thanks for playing. Goodbye.");
+    }
+
+    public static void printWelcome() {
+        System.out.println("------------------------------------");
+        System.out.println("NUMBER GUESSING GAME");
+        System.out.println("-------------------------------------");
+    }
+
+    public static int chooseDifficulty() {
+        while (true) {
+            System.out.println("Choose difficulty");
+            System.out.println("1. Easy (1-50)");
+            System.out.println("2. Normal(1-100)");
+            System.out.println("3. Hard(1-500)");
+            System.out.print("Enter your choice: ");
+            if (!scanner.hasNextInt()) {
+                System.out.println("Please enter valid number.");
+                scanner.next();
+                continue;
+            }
+            int choice = scanner.nextInt();
+            if (choice == 1) return 50;
+            if (choice == 2) return 100;
+            if (choice == 3) return 500;
+            System.out.println("Invalid choice. Try again.");
+        }
+    }
+
+    public static int getMaxAttempts(int maxNumber) {
+        if (maxNumber == 50) return 7;
+        else if (maxNumber == 100) return 10;
+        else return 15;
+    }
+
+    public static Integer readGuess(int maxNumber) {
+        System.out.print("Enter your guess: ");
+        if (!scanner.hasNextInt()) {
+            System.out.println("Please enter valid number.");
+            scanner.next();
+            return null;
+        }
+        int guess = scanner.nextInt();
+        if (guess < 1 || guess > maxNumber) {
+            System.out.println("Warning: Number must be between 1 and " + maxNumber);
+            return null;
+        }
+        return guess;
+    }
+
+    public static void printWarmHint(int secretNumber, int guess) {
+        int difference = Math.abs(secretNumber - guess);
+        if (difference <= 5) {
+            System.out.println("Very close!");
+        } else if (difference <= 10) {
+            System.out.println("Close!");
+        } else {
+            System.out.println("Far away");
+
+        }
+
+    }
+
+    public static boolean playGame() {
+        int maxNumber = chooseDifficulty();
+        int maxAttempts = getMaxAttempts(maxNumber);
+        int secretNumber = random.nextInt(maxNumber) + 1;
+        int attempts = 0;
+        boolean guessedCorrectly = false;
+        System.out.println("You have chosen a number between 1 and " + maxNumber);
+        System.out.println("You have " + maxAttempts + " Attempts");
+        while (attempts < maxAttempts) {
+            Integer guess = readGuess(maxNumber);
+            if (guess == null) {
+                continue;
+            }
+            attempts++;
+            if (guess == secretNumber) {
+                System.out.println("Correct! You guessed it in " + attempts + " attempts.");
+                guessedCorrectly = true;
+
+                return askReplay();
+            }
+            if (guess > secretNumber) {
+                System.out.println("Too high!");
+            } else {
+                System.out.println("Too low!");
+            }
+
+            printWarmHint(secretNumber, guess);
+            System.out.println("Attempts remaining: " + (maxAttempts - attempts));
+        }
+        System.out.println("Game Over! The correct number was: " + secretNumber);
+        return false;
+    }
+    public static boolean askReplay(){
+        System.out.print("Do you want to play game again(y/n)");
+        String answer = scanner.next().trim().toLowerCase();
+        if(answer.equals("y")){
+            return true;
+        }
+        return false;
+    }
+}
